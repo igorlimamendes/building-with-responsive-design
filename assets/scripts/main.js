d7ac87ea1ce82af093be96ed3c9646f3205441fc
igorlimamendes <igorlimamendes@protonmail.com>
2021-03-11 10:23:44 -0300

    Add code

---
+++ assets/scripts/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const wrapperBox1 = document.querySelector(".wrapper__box1");
+    const navLogo = document.querySelector(".nav__logo");
+    const navLogoSvg = document.querySelector(".nav__logo--svg");
+    
+    function navEffect() {
+        if (window.scrollY > 32 && window.innerWidth >= 768) {
+            wrapperBox1.classList.add("navEffect-box1");
+            navLogo.classList.add("navEffect-logo");
+        } else {
+            wrapperBox1.classList.remove("navEffect-box1");
+            navLogo.classList.remove("navEffect-logo");
+        }
+
+        if (window.scrollY > 32 && window.innerWidth > 768) {
+            navLogoSvg.classList.add("navEffect-logo-transition");
+        } else {
+            navLogoSvg.classList.remove("navEffect-logo-transition");
+        }
+    }
+    
+    window.addEventListener("scroll", navEffect);
+});
